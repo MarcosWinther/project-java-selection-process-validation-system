4f1b3c1ae413ac492f1741b7cadf6dd0b31c2cb2
MarcosWinther <winthermarcos@gmail.com>
2024-08-19 11:53:41 -0300

    :sparkles: feat: Implementada a função de seleção de candidatos

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,42 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
 		System.out.println("Processo Seletivo");
 		
-		analisarCandidato(1900.0);
-		analisarCandidato(2500.0);
-		analisarCandidato(2000.0);
+		//analisarCandidato(1900.0);
+		//analisarCandidato(2500.0);
+		//analisarCandidato(2000.0);
+		
+		selecaoCandidatos();
+	}
+	
+	static void selecaoCandidatos() {
+		
+		String [] candidatos = {"Marcos", "Thais", "Luana", "Sabrina", "William", "Larissa", "Isabela", "Levy", "Cesár", "Aida"};
+		
+		int candidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase = 2000.0;
+		
+		while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O(A) candidato(a) " + candidato + " solicitou este valor de salário: R$ " + salarioPretendido);
+			
+			if(salarioBase >= salarioPretendido) {
+				System.out.println("O(A) candidato(a) " + candidato + " foi selecionado(a) para a vaga.");
+				candidatosSelecionados++;
+			}
+			candidatoAtual++;
+		}
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 	
 	static void analisarCandidato(double salarioPretendido) {
