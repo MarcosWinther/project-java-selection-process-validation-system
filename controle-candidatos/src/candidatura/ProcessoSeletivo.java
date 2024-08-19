1a5ed58bbd72d8591d82f5f6fff473df64aded30
MarcosWinther <winthermarcos@gmail.com>
2024-08-19 12:55:34 -0300

    :sparkles: feat: Implementado a função de fazer ligações para cada candidato selecionado

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,18 +1,43 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
 		System.out.println("Processo Seletivo");
 		
-		//analisarCandidato(1900.0);
-		//analisarCandidato(2500.0);
-		//analisarCandidato(2000.0);
+		String [] candidatos = {"Marcos", "Thais", "Luana", "Sabrina", "William", "Larissa", "Isabela", "Levy", "Cesár", "Aida"};
 		
-		//selecaoCandidatos();
+		for(String candidato : candidatos) {
+			entrandoEmContato(candidato);
+		}
+	}
+	
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
 		
-		imprimirSelecionados();
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			
+			if(continuarTentando)
+				tentativasRealizadas ++;
+			else
+				System.out.println("CONTATO REALIZADO COM SUCESSO!");
+			
+		} while(continuarTentando && tentativasRealizadas < 3);
+		
+		if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "ª TENTIVA.");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE LIGAÇÕES " + tentativasRealizadas);
+	}
+	
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
 	}
 	
 	static void imprimirSelecionados() {	
